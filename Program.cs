3a4cab8e62730b6511d686406d2fe1a2c9d1ce90
MagelGD <miguel0504@live.com>
2025-01-10 01:09:38 -0500

    Se ajusta la arquitectura del api y se empieza a crear los repositories

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
+using Api.Data.Models;
+using Api.Data.Repositories.ConfigurarRepositories;
+using Api.Services.ConfigurarServices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +12,52 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+#region CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:44373") // Especifica el origen permitido
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials(); // Permite cookies, autenticación, etc.
+        });
+});
+#endregion
+
+#region Registrar DbContext
+builder.Services.AddDbContext<UniversidadContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+#endregion
+
+
+#region Services
+builder.Services.AddServices();
+#endregion
+#region Repositories
+builder.Services.AddRepositories();
+#endregion
+
+#region Entornos
+// Establece el entorno según la variable de entorno (Development, Staging, Production)
+var environment = builder.Environment.EnvironmentName;
+
+// Esto ya carga appsettings.json y appsettings.{Environment}.json automáticamente
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+#endregion
+#region SwaggerConfig
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+});
+#endregion
 
 var app = builder.Build();
 
@@ -17,6 +69,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
